a4e63c9344c325eaa3b87d92aa344079a750ac93
alexsanch3zx <asanche2@trincoll.edu>
2025-12-12 18:06:03 -0500

    commands to run

--- build-push.sh
+++ build-push.sh
@@ -11,3 +11,13 @@ docker push harbor.javajon.duckdns.org/library/found-workout-service:v1.0
 docker push harbor.javajon.duckdns.org/library/found-challenge-service:v1.0
 
 docker push harbor.javajon.duckdns.org/library/found-data-consistency-service:v1.0
+
+
+# New images if you can push for us.
+docker build -t harbor.javajon.duckdns.org/library/found-auth-service:v1.0 ./services/auth-service 
+docker push harbor.javajon.duckdns.org/library/found-auth-service:v1.0 
+
+docker build -t harbor.javajon.duckdns.org/library/found-user-service:v1.0 ./services/user-service 
+docker push harbor.javajon.duckdns.org/library/found-user-service:v1.0
+
+
