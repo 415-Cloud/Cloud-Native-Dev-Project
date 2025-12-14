b02d6d2c4c1ebae8d985220771d1927a0614e4ae
alexsanch3zx <asanche2@trincoll.edu>
2025-12-13 20:00:27 -0500

    added found-prefix

--- build-push.sh
+++ build-push.sh
@@ -14,10 +14,10 @@ docker push harbor.javajon.duckdns.org/library/found-data-consistency-service:v1
 
 
 # New images if you can push for us.
-docker build -t harbor.javajon.duckdns.org/library/found-auth-service:v1.0 ./services/auth-service 
+docker build --platform linux/amd64 -t harbor.javajon.duckdns.org/library/found-auth-service:v1.0 ./services/auth-service 
 docker push harbor.javajon.duckdns.org/library/found-auth-service:v1.0 
 
-docker build -t harbor.javajon.duckdns.org/library/found-user-service:v1.0 ./services/user-service 
+docker build --platform linux/amd64 -t harbor.javajon.duckdns.org/library/found-user-service:v1.0 ./services/user-service 
 docker push harbor.javajon.duckdns.org/library/found-user-service:v1.0
 
 
