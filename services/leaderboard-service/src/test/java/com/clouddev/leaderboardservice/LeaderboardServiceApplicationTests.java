125351a1a079907b720ca823468e1689cace5f0a
shanomac11 <shaneivey22@gmail.com>
2025-12-15 00:31:52 -0500

    imported jUnit assertion

--- services/leaderboard-service/src/test/java/com/clouddev/leaderboardservice/LeaderboardServiceApplicationTests.java
+++ services/leaderboard-service/src/test/java/com/clouddev/leaderboardservice/LeaderboardServiceApplicationTests.java
@@ -1,7 +1,8 @@
 package com.clouddev.leaderboardservice;
 
-
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
+
 /**
  * Basic application test.
  * Note: Full @SpringBootTest disabled due to JaCoCo incompatibility with Java 25.
